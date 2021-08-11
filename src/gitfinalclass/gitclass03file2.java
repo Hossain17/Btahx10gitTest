6cc0d3e362a32748b9358b5057a9562c2beead9f
Hossain17 <imam09hossain@gmail.com>
2021-08-12 05:46:02 +0600

    the second commit

--- src/gitfinalclass/gitclass03file2.java
+++ src/gitfinalclass/gitclass03file2.java
@@ -1,11 +1,12 @@
 package gitfinalclass;
 
-public class gitfinalclass03 {
+public class gitclass03file2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
+	System.out.println("the is the second commit");	
 		
 		
 		
